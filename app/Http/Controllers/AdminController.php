d23b2d6ac358289abb7139d17a07a3afd90d5d18
pswebcom <pswebco@gmail.com>
2022-12-21 00:36:55 -0500

    edit profile completed ../admin/profile

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -25,16 +25,38 @@ class AdminController extends Controller
     {
         $id = Auth::user()->id;
         $adminData = User::find($id);
-        return view('admin.admin_profile_view',compact('adminData'));
+        return view('admin.admin_profile_view', compact('adminData'));
     }
 
     public function EditProfile()
     {
         $id = Auth::user()->id;
         $editData = User::find($id);
-        return view('admin.admin_profile_edit',compact('editData'));
+        return view('admin.admin_profile_edit', compact('editData'));
     }
 
+    public function StoreProfile(Request $request)
+    {
+        $id = Auth::user()->id;
+        $data = User::find($id);
 
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+
+        if ($request->file('profile_image')) {
+            $file = $request->file('profile_image');
+
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $filename);
+            $data['profile_image'] = $filename;
+
+        }
+
+        $data->save();
+
+        return redirect()->route('admin.profile');
+
+    }
 
 }
--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -8,7 +8,10 @@
                         <div class="card-body">
 
                             <h4 class="card-title">Edit Profile Page</h4>
-                            <form action="">
+                            <form method="post" action="{{ route('store.profile') }}" enctype="multipart/form-data">
+
+                                @csrf
+
                                 <div class="row mb-3">
                                     <label for="example-text-input" class="col-sm-2 col-form-label">User Name</label>
                                     <div class="col-sm-10">
